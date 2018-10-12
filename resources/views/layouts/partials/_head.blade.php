32ce278d5f8db39dcefb66125ed814abfdaebb5c
Tam Jid <tamjid.mdh@gmail.com>
2018-10-12 17:24:23 +0600

    first commit

---
+++ resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="{{asset('favicon.ico')}}">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://127.0.0.1:8888/public/cdn/bootstrap-4/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/dashboard.css')}}" rel="stylesheet">
+</head>
---
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -0,0 +1,45 @@
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="http://127.0.0.1:8888/public/cdn/jquery/v3.3.1/jquery-3.3.1.min.js"></script>
+<script src="http://127.0.0.1:8888/public/cdn/jquery-slim/js/jquery-slim.min.js"></script>
+<script src="http://127.0.0.1:8888/public/cdn/popper/popper.min.js"></script>
+<script src="http://127.0.0.1:8888/public/cdn/bootstrap-4/js/bootstrap.min.js"></script>
+
+<!-- Icons -->
+<script src="http://127.0.0.1:8888/public/cdn/feather/feather.min.js"></script>
+<script>
+    feather.replace()
+</script>
+
+<!-- Graphs -->
+<script src="http://127.0.0.1:8888/public/cdn/chart/Chart.min.js"></script>
+<script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+            datasets: [{
+                data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+                lineTension: 0,
+                backgroundColor: 'transparent',
+                borderColor: '#007bff',
+                borderWidth: 4,
+                pointBackgroundColor: '#007bff'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: false,
+            }
+        }
+    });
+</script>
